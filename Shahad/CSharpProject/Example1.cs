bba3bb284a8515c50bdf90e58b6c907db337dc85
Shahad Chandra Shrestha <i260008@d2hs.com>
2019-11-06 19:01:06 +0545

    Added files for Arrays and Iterations

---
+++ Shahad/CSharpProject/Example1.cs
@@ -0,0 +1,13 @@
+using CSharpProject;
+public class Example1
+{
+    public void test()
+    {
+        int j = 1;
+        for (int k = 5; j < 10; k=k+5)
+        {
+            System.Console.Write(k + " ");
+            j++;
+        }
+    }
+}
---
+++ Shahad/CSharpProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Example1 Obj1 = new Example1();
+            Obj1.test();
+
+        }
+    }
+}
