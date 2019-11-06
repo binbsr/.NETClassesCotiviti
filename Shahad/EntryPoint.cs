b4731f45fcc5ea74f0234114dd20f65833fa56aa
Shahad Chandra Shrestha <i260008@d2hs.com>
2019-11-06 11:24:33 +0545

    Entry point file

---
+++ Shahad/EntryPoint.cs
@@ -0,0 +1,32 @@
+public class 
+{
+    public void Main()
+    {
+        Person p = new Person();
+        p.Read()
+
+    }
+}
+
+
+
+public class Person
+{
+    //Data
+    string firstName;
+    string lastName;
+    int age;
+    string addres;
+
+    internal void Read()
+    {
+
+    }
+
+    void Walk()
+    {
+
+    }
+
+
+}
