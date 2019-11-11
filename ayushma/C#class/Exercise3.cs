28a98829fdbd6880fc2326027f3e23a86ad089ad
Ayushma Amatya <i260013@d2hs.com>
2019-11-11 15:48:04 +0545

    Added project for the topic methods

---
+++ ayushma/C#class/Exercise3.cs
@@ -0,0 +1,15 @@
+using System;
+public class Exercise3
+{
+public void LearnNestedLoops()
+    {
+        for(int i=0; i<5; i++)
+        {
+            Console.WriteLine("Jai");
+            for (int j=0; j<10; j++)
+            {
+                Console.WriteLine("Nepal");
+            }
+        }
+    }
+}
