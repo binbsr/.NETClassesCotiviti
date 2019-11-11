b5d78723ddadb46481e2b57f9962082d10ce7a02
Ayushma Amatya <i260013@d2hs.com>
2019-11-11 17:43:53 +0545

    Added project for the methods and arguments

--- ayushma/C#class/EntryPoint.cs
+++ ayushma/C#class/EntryPoint.cs
@@ -23,18 +23,27 @@ public static void Main() // main methods has to be static
         //obj2.UseTernaryEvenOdd();
         //obj2.SumofSquares();
         
-        //Exercise3 obj3 = new Exercise3();
+        Exercise3 obj3 = new Exercise3();
         //obj3.LearnNestedLoops();
-        Methods m = new Methods();
+        //Methods m = new Methods();
         //long sum = m.AddTwoNumbers(273642,463729);
         //Console.WriteLine(sum);
         //Console.WriteLine ("Sum is " + sum);
         //named arguments
         //string fullname = m.GetFullName(lastname: "Amatya", firstName: "Ayushma");
         //Console.WriteLine (fullname);
-        (int Min,int Max) result = m.GetMinMax(new int[] {4,3,5,2,6});
+        //(int Min,int Max) result = m.GetMinMax(new int[] {4,3,5,2,6});
         //Console.WriteLine (result);
-        Console.WriteLine($"Minimum value = {result.Min} and Maximum value = {result.Max}");
+        //Console.WriteLine($"Minimum value = {result.Min} and Maximum value = {result.Max}");
+        double avg = obj3.AverageofDecimalNumbers(12.5, 12.5, 12.5);
+        Console.WriteLine(avg);
+
+        Exercise3 obj4 = new Exercise3();
+        long sum = obj4.AverageofN(8,8,8,8,8);
+        Console.WriteLine(sum);
+
+        Exercise3 obj5=new Exercise3();
+        obj5.CountGender();
     }
 }
 }
