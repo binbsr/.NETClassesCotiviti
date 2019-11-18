0c04102837435ec91cbe3ba37f9250fd9090cac2
kc-bikram <bikram.kc@cotiviti.com>
2019-11-18 12:54:19 +0545

    Properties and Constructor Examples

---
+++ BikramKC/DotNetClass/ConstantsNEnums.cs
@@ -0,0 +1,18 @@
+namespace Properties
+{
+    public class Customer
+    {
+        public const string cardNumber = "123XXX49498";
+        public readonly string citizenID;
+        public Customer()
+        {
+            citizenID = "132/4545";
+        }
+        enum weekDay {Sunday, Monday, Tuesday, Wednesday, Thrusday, Friday, Saturday};
+        public void EnumExample(string x)
+        {
+            
+        }
+
+    }
+}
---
+++ BikramKC/DotNetClass/Person.cs
@@ -0,0 +1,9 @@
+class Person
+{
+    private string name;
+    private byte age;
+    private static string sceintificName = "Homo Sapiens";
+    public Person()
+    {}
+    public Person(string nam) => name = nam;
+}
