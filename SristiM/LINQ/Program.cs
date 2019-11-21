43148b4e4d1d50cce61823618cd1287cd9dc3ab9
Sristi Musyaju <i80685@d2hs.com>
2019-11-21 14:31:02 +0545

    Added class details-Nov21-LINQ

---
+++ SristiM/LINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ {
+    class Program {
+        static void Main (string[] args) {
+            //LINQ-Language Integrated Query
+            //Example-1
+            string[] names = { "Sristi", "Ayushma", "Shreya" };
+            var orderedNames = names.OrderBy (x => x); //lamda expression, arrow operation, x (individual item)goes to x as there is one parameter 
+            var namesFiltered = names.Where (x => x.StartsWith ("S")); //filter name starting with S
+
+            foreach (var name in namesFiltered) {
+                Console.WriteLine (name);
+
+            }
+
+            //Example-2
+            var students = Collections.FetchStudent ();
+            var result = from student in students
+            //where students.CollgeName="KU"
+            group student by student.CollegeName into studentGroup
+            orderby studentGroup.Key
+            select studentGroup;
+
+            foreach (var x in result) {
+                //System.Console.WriteLine (x.Key);
+                foreach (var s in x) {
+                    System.Console.WriteLine (x.Key+"--" + s.Name);
+
+                }
+
+            }
+        }
+    }
+}
