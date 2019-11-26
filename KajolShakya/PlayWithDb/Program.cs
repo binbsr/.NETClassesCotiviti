3d609613c61099773272ffb6295894bc543ba46e
KajolShakya123 <kajol.shakya@cotiviti.com>
2019-11-26 15:21:01 +0545

    nov26 EntityFramework

---
+++ KajolShakya/PlayWithDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayWithDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
