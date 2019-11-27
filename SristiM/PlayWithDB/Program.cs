67daa64e6190176ab6191f0ad505f51dfd8bff1a
Sristi Musyaju <i80685@d2hs.com>
2019-11-27 09:43:01 +0545

    Added Nov-26 class details- Entity Framework

---
+++ SristiM/PlayWithDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Entity Framework--windows based, not open source
+//Entity Framework Core--cross platform, open source
+
+namespace PlayWithDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
