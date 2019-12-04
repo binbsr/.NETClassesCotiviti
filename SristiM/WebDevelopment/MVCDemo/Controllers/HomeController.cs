c1a2451fdac69387b4d97250194b17eefa917116
Sristi Musyaju <i80685@d2hs.com>
2019-12-04 14:22:33 +0545

    added Dec-4 classwork/HW -MVCDemo

---
+++ SristiM/WebDevelopment/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers {
+    public class HomeController : Controller {
+        //web page will be controllername\action i.e. Home\Index
+        public IActionResult Index () {
+            return View ();
+        }
+
+        public IActionResult Gallery () => View (); //inline method so using lamda expression
+
+    }
+}
