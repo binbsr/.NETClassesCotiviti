191361e3eb44919fe6f90332054648bf7a9d4d53
kc-bikram <bikram.kc@cotiviti.com>
2019-12-04 14:08:03 +0545

    Home + class work Dec-4

--- BikramKC/DotNetClass/Student.cs
+++ BikramKC/DotNetClass/Student.cs
@@ -1,13 +1,9 @@
 public class Student
 {
-    public int rollNo;
-    public string name;
-    public string address;
-    public string stream;
-    public static string collegeName;
-    public static string GetCollegeName()
-    {
-        return collegeName;
-    }
-    
+    public int RollNo{get; set;}
+    public string Name{get; set;}
+    public string Address{get; set;}
+    public string Stream{get; set;}
+    public static string CollegeName{get; set;}
+    public string FacultyID {get; set;}    
 }
